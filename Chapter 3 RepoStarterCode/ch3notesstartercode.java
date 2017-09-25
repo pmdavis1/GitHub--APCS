aa518f27a21279329de12a84d15b5c1ece8e50a3
pmdavis1 <31441846+pmdavis1@users.noreply.github.com>
2017-09-25 08:33:47 -0500

    9/25/17

---
+++ Chapter 3 RepoStarterCode/ch3notesstartercode.java
@@ -0,0 +1,65 @@
+/********************************************************************
+// These are the notes for ch 3 used in hayes class
+
+
+import
+//******************************************************************* */
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+//import cs1.Keyboard;
+import javax.swing.*;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+
+public class ch3notesstartercode
+{
+public static void main (String [] args)
+{
+
+/**/
+	String word1 = "apple";
+	String word2 = "cat";
+	String word3 = "dog";
+
+	if(word2.compareTo(word1)<0)//;end a statement BAD BAD simicolen
+	{
+		System.out.println(word2 +" is before " + word1  );
+		System.out.println("and cats are evil" );
+	}//ctrl M - will match brackets
+	else
+		System.out.println(word1 +" is before " + word2  );
+
+	if(word2.compareTo(word3)<0)
+		System.out.println(word2 +" is before " + word3  );
+/*
+
+      int num = 5, num1 =10, num2 = 4;  // standard hours in a work week
+
+      System.out.print ("Enter a number ");
+      num = Keyboard.readInt();
+
+       System.out.print ("Enter a  second number ");
+	   num1 = Keyboard.readInt();
+/*
+       System.out.print ("Enter a  third number ");
+	   num2 = Keyboard.readInt();
+
+      System.out.println ();
+
+
+/*  */
+
+}// end of main
+
+
+}// end of class
+
+
+
+
+
+
+
+
+
